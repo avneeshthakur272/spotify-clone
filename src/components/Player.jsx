e81fc3a40f79371e94895f73036636ab3a1c3174
Avneesh thakur <avneeshhakur272@gmail.com>
2024-07-03 01:14:56 +0530

    adding onclick property on songs and add functionality of seekBar and songs time.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 import { PlayerContext } from '../context/PlayerContest'
 
 const player = () => {
-  const {track,seekbg,seekbar,playStatus,play,pause} =useContext(PlayerContext)
+  const {track,seekbg,seekBar,playStatus,play,pause,time} =useContext(PlayerContext)
 
   return (
     <div className='h-[10%] bg-black flex justify-between item-center text-white px-4'>
@@ -25,11 +25,11 @@ const player = () => {
           <img className='w-4 cursor-pointer' src={assets.loop_icon} alt="" />
         </div>
         <div className='flex item-center gap-5'>
-          <p>1:06</p>
+          <p>{time.currentTime.minute}:{time.currentTime.second}</p>
           <div ref={seekbg} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer h-1'>
-            <hr ref={seekbar} className='h-1 border-none w-20 bg-green-800 rounded-full' />
+            <hr ref={seekBar} className='h-1 border-none w-0 bg-green-800 rounded-full' />
           </div>
-          <p>3:20</p>
+          <p>{time.totalTime.minute}:{time.totalTime.second}</p>
         </div>
       </div>
       <div className='hidden lg:flex items-center gap-2 opacity-75'>
--- src/context/PlayerContest.jsx
+++ src/context/PlayerContest.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext()
@@ -31,6 +31,33 @@ const PlayerContextProvider = (props) => {
         setPlayStatus(false)
     }
 
+    const playWithId = async(id)=> {
+        await setTrack(songsData[id])
+        await audioRef.current.play()
+        setPlayStatus(true)
+    }
+
+    useEffect(()=> {
+        setTimeout(() => {
+
+            audioRef.current.ontimeupdate = ()=> {
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%";
+                setTime({
+                currentTime:{
+                    second:Math.floor(audioRef.current.currentTime % 60),
+                    minute:Math.floor(audioRef.current.currentTime / 60)
+                },
+                totalTime:{
+                    second:Math.floor(audioRef.current.duration % 60),
+                    minute:Math.floor(audioRef.current.duration / 60)
+                }
+
+            })
+         }
+            
+        }, 1000);
+    })
+
     const contextValue ={
         audioRef,
         seekBg,
@@ -38,7 +65,8 @@ const PlayerContextProvider = (props) => {
         track,setTrack,
         playStatus,setPlayStatus,
         time,setTime,
-        play,pause
+        play,pause,
+        playWithId
     }
 
     return (
